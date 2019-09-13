6a17a1b43e7ffc134ffe0ba6cd523251c49ccf80
TJ Jackson <elitefourfresh@gmail.com>
2019-09-12 23:47:45 -0400

    cleaning things up a little

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
     let playerScore = 0;
     let computerScore = 0;
     let tieScore = 0;
-    let message = "";
+    // let message = "";
+    let resultMessage = document.querySelector('#resultMessage');
     var rockBtn = document.querySelector('#rock');
     var paperBtn = document.querySelector('#paper');
     var scissorsBtn = document.querySelector('#scissors');
@@ -12,7 +13,7 @@
 
 // FUNCTIONS
 
-    function computerPlay() {
+function computerPlay() {
     const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random()*choice.length)];
 };
@@ -36,59 +37,65 @@ function playRound(playerSelection, computerSelection) {
     switch (playerSelection + computerSelection) {
         case "rockrock":
             tie();
-            return document.getElementById('resultMessage').innerHTML = "Hey, it's a draw! Play again!";
+            return resultMessage.innerHTML = "Hey, it's a draw! Play again!";
             break;
         case "rockpaper":
             lose();
-            return document.getElementById('resultMessage').innerHTML = "Sorry! Paper beat rock, you lose!";
+            return resultMessage.innerHTML = "Sorry! Paper beat rock, you lose!";
             break;
         case "rockscissors":
             win();
-            return document.getElementById('resultMessage').innerHTML = "Rock crushes scissors! YOU WIN!";   
+            return resultMessage.innerHTML = "Rock crushes scissors! YOU WIN!";   
             break;
         case "paperpaper":
             tie();
-            return document.getElementById('resultMessage').innerHTML = "Hey, it's a draw! Play again!";
+            return resultMessage.innerHTML = "Hey, it's a draw! Play again!";
             break;
         case "paperscissors":
             lose();
-            return document.getElementById('resultMessage').innerHTML = "Woah! Scissors cut paper, you lose!";
+            return resultMessage.innerHTML = "Woah! Scissors cut paper, you lose!";
             break;
         case "paperrock":
             win();
-            return document.getElementById('resultMessage').innerHTML = "Oh my goodness. Paper covers rock...YOU WIN!";
+            return resultMessage.innerHTML = "Oh my goodness. Paper covers rock...YOU WIN!";
             break;
         case "scissorsscissors":
             tie();
-            return document.getElementById('resultMessage').innerHTML = "Hey, it's a draw! Play again!";
+            return resultMessage.innerHTML = "Hey, it's a draw! Play again!";
             break;
         case "scissorsrock":
             lose();
-            return document.getElementById('resultMessage').innerHTML = "Scissors stand no chance against the mighty rock! You lose!";
+            return resultMessage.innerHTML = "Scissors stand no chance against the mighty rock! You lose!";
             break;
         case "scissorspaper":
             win();
-            return document.getElementById('resultMessage').innerHTML = "Scissors absolutely decapitate paper! YOU WIN!";
+            return resultMessage.innerHTML = "Scissors absolutely decapitate paper! YOU WIN!";
             break;
         default:
-            console.log("Something went wrong.");
+            return resultMessage.innerHTML = "Something went wrong.";
+            // console.log("Something went wrong.");
     }
 
 }
 
+// EVENT LISTENERS
+
 function main() {
     rockBtn.addEventListener('click', () => {
-        console.log(playRound('rock', computerPlay()));
+        // console.log(playRound('rock', computerPlay()));
+        playRound('rock', computerPlay());
         endGame();
     });
 
     paperBtn.addEventListener('click', () => {
-        console.log(playRound('paper', computerPlay()));
+        // console.log(playRound('paper', computerPlay()));
+        playRound('paper', computerPlay());
         endGame();
     });
 
     scissorsBtn.addEventListener('click', () => {
-        console.log(playRound('scissors', computerPlay()));
+        // console.log(playRound('scissors', computerPlay()));
+        playRound('scissors', computerPlay());
         endGame();
     });
 };
@@ -98,15 +105,16 @@ main();
 function endGame() {
     if (playerScore === 5 || computerScore === 5) {
         if (playerScore > computerScore) {
-            return document.getElementById('resultMessage').innerHTML = "You have defeated the almighty CPU in a best to 5 contest! Congrats!!!";
+            return resultMessage.innerHTML = "You have defeated the almighty CPU in a best to 5 contest! Congrats!!!";
         } else {
-            return document.getElementById('resultMessage').innerHTML = "You have BEEN defeated by the almighty CPU overload. Better luck next time.";
-        } 
-    }
-}
+            return resultMessage.innerHTML = "You have BEEN defeated by the almighty CPU overload. Better luck next time.";
+        };
+        reset();
+    };
+};
 
 function reset() {
     playerScore = 0;
     computerScore = 0;
     tieScore = 0;
-}
+};
